84908789338e6c9ee86c45e4a92679e47617e247
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2023-08-24 20:10:53 +0530

    Add files via upload

---
+++ ASCII_value_of_char.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("\n\n\t\tWelcome to C Programming\n\n");
+    char c;
+    printf("\tEnter any character : ");
+    scanf("%c",&c);
+    printf("\n\tThe ASCII value of %c is %d ",c,c);
+    return 0;
+}
